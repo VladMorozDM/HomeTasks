447bc9b728a67bead6f2856991b467c74c0a39b1
Moroz Vladislav <nikeblack97@gmail.com>
2018-03-24 22:55:07 +0200

    hw04 accordion js + fix css

--- homework04/js/main.js
+++ homework04/js/main.js
@@ -1,9 +1,7 @@
 (()=> {
-    console.log(document.images)
-
      'use strict';
 let $ = jQuery;
-
+// -------------------------------OWL CAROUSEL------------------------
     let owl = $('.owl-carousel');
 
      $('.owl-carousel').owlCarousel({
@@ -34,6 +32,43 @@ let $ = jQuery;
         owl.trigger('prev.owl.carousel', [300]);
     })
 
+    // -------------------------------OWL CAROUSEL------------------------
+    // -------------------------------ACCORDION---------------------------
+    $( ".accordion" ).accordion({
+        active: "none",
+        collapsible: true,
+        heightStyle: "none"
+        });
+
+    $('.ui-accordion .ui-accordion-header .ui-accordion-header-icon').css({
+        "left": "11.5em"
+    });
+
+    let changeHeight = document.getElementById('select-top');
+    let changeHeightIphone = document.getElementById('select-top-iphone');
+    let changePosition = document.getElementById('select-cart');
+    // console.log(changeHeight);
+    let counter = 0;
+    changeHeight.addEventListener('click', function(){
+
+        if (!counter) {
+            changePosition.style.height = '86px';
+            changePosition.style.position = 'relative';
+            changePosition.style.top = '0';
+            counter++;
+        } else {
+            console.log(changePosition.style.height);
+            changePosition.style.height = '40px';
+            console.log(changePosition.style.height);
+            changePosition.style.position = 'static';
+            counter = 0;
+
+        }
+
+
+    });
+
+    // -------------------------------ACCORDION---------------------------
 })();
 
 
